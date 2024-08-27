60906c4cfb843d707dfb914e6042ecff3dbcf15f
CHOOSEIT <chooseitytt@gmail.com>
2024-08-27 12:04:24 +0200

    fix: remove additional prints

--- workflow.py
+++ workflow.py
@@ -1,5 +1,4 @@
 from story import Story
-from agents.illustratorAgent import query_suggested_illustrations
 from openaiAPI import openai_show_usage
 
 story = Story(need_illustration=False, story_length=3)
@@ -17,9 +16,3 @@ while code_error == 0 or code_error == 1:
         story.input_user_answer(user_input)
 
 openai_show_usage()
-
-print("Final formatted story")
-print(story.get_formatted_story())
-
-print("Final story")
-print(story.get_story())
